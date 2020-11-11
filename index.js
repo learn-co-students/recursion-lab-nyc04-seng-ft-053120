620940930ba70a2dc15f8cb8be382862d39dd031
kceron <karemiceron@gmail.com>
2020-11-10 20:27:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,55 +1,45 @@
-// Code your solution here!
-function printString(string) {
-    if (string.length === 1) {
-        return
+function printString(str) {
+    if (str.length === 1) {
+      console.log(str[0])
+      return;
     } else {
-        printString(string.slice(1))
+      console.log(str[0])
+      return printString(str.slice(1))
     }
-}
-
-function reverseString(string, index = string.length - 1) {
-    if (index === 0) {
-        return string[index]
+  }
+  
+  function reverseString(str) {
+    return (str.length === 1) ? str : reverseString(str.slice(1)) + str[0]
+  }
+  
+  function isPalindrome(str) {
+    if (str.length <= 1) {
+      return true
     } else {
-        return string[index] + reverseString(string, index - 1)
+      if (str[0] !== str[str.length - 1]) return false;
+      return isPalindrome(str.slice(1, str.length - 1))
     }
-}
-
-function isPalindrome(string) {
-    if (string[0] !== string[string.length-1]) {
-        return false
-    }
-
-    if (string.length === 1) {
-        return true
+  }
+  
+  function addUpTo(arr, idx) {
+    // sums all members up to a given index in an array
+    if (idx === 0) return arr[0];
+    return addUpTo(arr, idx - 1) + arr[idx]
+  }
+  
+  function maxOf(arr) {
+    // finds largest int of an array
+    if (arr.length <= 1) return arr[0];
+    return Math.max(arr.pop(), maxOf(arr))
+  }
+  
+  function includesNumber(arr, num) {
+    // returns true if num is present in arr
+  
+    if (arr.length === 1) {
+      return arr[0] === num;
     } else {
-        return isPalindrome(string.slice(1, string.length-1))
+      if (arr[0] === num) return true;
+      return includesNumber(arr.slice(1), num)
     }
-}
-
-function addUpTo(array, target, index = 0) {
-    if (index === target) {
-        return array[target]
-    } else {
-        return array[index] + addUpTo(array, target, index + 1)
-    }
-}
-
-function maxOf(array) {
-    if (array.length === 1) {
-        return array[0]
-    } else {
-        return Math.max(array[0], maxOf(array.slice(1)))
-    }
-}
-
-function includesNumber(array, target) {
-    if (array[0] === target) {
-        return true
-    }
-    if (array.length === 1) {
-        return false
-    } else {
-        return includesNumber(array.slice(1), target)
-    }
-}
+  }
