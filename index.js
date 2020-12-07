01366f1ea5d2587c7c8d6e9b73c94a7a9951ffde
garcikg0 <kg.gar91@gmail.com>
2020-12-07 17:10:45 -0500

    Done.

--- index.js
+++ index.js
@@ -9,4 +9,37 @@ function printString(myString) {
     }
 }
 
+function reverseString(string) {
+    if (string.length <= 1) {
+        return string;
+    }
+    return reverseString(string.substring(1)) + string[0]
+}
 
+function isPalindrome(string) {
+   if ( string.length === 1 ) return true;
+   if (string[0] !== string[string.length - 1]) return false;
+   
+   return isPalindrome(string.substring(1, string.length - 1))
+}
+
+function addUpTo(arr, idx) {
+    if (idx === 0) {
+        return arr[idx];
+    }
+    return arr[idx] + addUpTo(arr, idx - 1);
+}
+
+function maxOf (arr, idx=0, max=arr[0]) {
+    if (idx === arr.length-1) return max;
+    if (arr[idx] > max) max = arr[idx];
+    return maxOf(arr, idx+1, max)
+}
+
+function includesNumber(arr, target, idx=0) {
+    if (idx > arr.length - 1) {
+        return false
+    }
+    if (arr[idx] === target) return true;
+    return includesNumber(arr, target, idx + 1)
+}
